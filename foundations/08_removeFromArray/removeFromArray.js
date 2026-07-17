8c77e375cff600109ae71af6f814a7be0e0fdf3f
sdas-137 <255654863+sdas-137@users.noreply.github.com>
2026-07-18 03:53:05 +0530

    Finish exercise foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // O(n) solution
+    let mp = new Map();
+    for (let arg of args) mp.set(arg, 1);
+    return arr.filter(item => !mp.get(item));
 };
 
 // Do not edit below this line

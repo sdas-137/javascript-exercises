a708c24ab87f4c1372eef2340e0f396899d7d585
sdas-137 <255654863+sdas-137@users.noreply.github.com>
2026-07-18 04:07:28 +0530

    Finish exercise foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return "ERROR";
+    let start = a < b ? a : b; 
+    let end = start == b ? a : b;
+    let sum = 0;
+    for (let i = start; i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
